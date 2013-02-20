cb9f87d2b59fcb02e12077e8ef086267201c7b54
heartsentwined <heartsentwined@cogito-lab.com>
2013-02-20 12:44:54 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberAuthRailsDemo::Application.config.secret_token = '77eb25423ebd50be1821438a8792aa68ed446bb7a40a4c7cf205fe1550e8ca5e5de90a77aedfe37b1bc429a23ff4ddc1c4c087fe46d8c3903dbb6da90ff773c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EmberAuthRailsDemo::Application.config.session_store :cookie_store, key: '_ember-auth-rails-demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EmberAuthRailsDemo::Application.config.session_store :active_record_store
