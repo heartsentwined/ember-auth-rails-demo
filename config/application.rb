863a1680978c439ff5174ed05eab894926171963
heartsentwined <heartsentwined@cogito-lab.com>
2013-02-20 13:35:19 +0800

    asset pipeline fix

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module EmberAuthRailsDemo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # heroku asset precompilation fix
+    config.assets.initialize_on_precompile = false
   end
 end
