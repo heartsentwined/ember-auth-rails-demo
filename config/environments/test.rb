9609403d6f5d100f1826ff4ed46733ff850766f6
heartsentwined <heartsentwined.me@gmail.com>
2013-10-18 16:26:46 +0800

    bcrypt for tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,10 @@ EmberAuthRailsDemo::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
